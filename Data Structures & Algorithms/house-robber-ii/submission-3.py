3e54ff7b25412a60af2ae363cc2e7ab1b4ad6b63
Ryan Tran <124413408+rtran381@users.noreply.github.com>
2026-09-25 17:14:57 -0700

    Add: house-robber-ii - submission-3

---
+++ Data Structures & Algorithms/house-robber-ii/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        def dfs(i, arr, hashmap):
+            if i >= len(arr):
+                return 0
+            if i in hashmap:
+                return hashmap[i]
+            hashmap[i] = max(arr[i] + dfs(i + 2, arr, hashmap), dfs(i + 1, arr, hashmap))
+            return hashmap[i]
+        return max(dfs(0, nums[1:], {}), dfs(0, nums[:-1], {}))
